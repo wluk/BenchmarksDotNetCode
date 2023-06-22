6a4c2b59d859b98c2bbfd22c2337a5498e570bbc
Lukasz Wielopolski <lukasz.wielopolski@outlook.com>
2023-06-22 08:49:19 +0200

    Contain span and string

---
+++ Span/ContainTests.cs
@@ -0,0 +1,27 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+
+namespace Span;
+
+[MemoryDiagnoser]
+[SimpleJob(RuntimeMoniker.Net60)]
+[SimpleJob(RuntimeMoniker.Net70)]
+[SimpleJob(RuntimeMoniker.Net80)]
+public class ContainTests
+{
+    private readonly string command = "CreateApplicationRoleCommand";
+
+    [Benchmark]
+    public bool StringContainString() => command.Contains("role", StringComparison.InvariantCultureIgnoreCase);
+
+    [Benchmark]
+    public bool SpanContainString() => command.AsSpan().Contains("role", StringComparison.InvariantCultureIgnoreCase);
+
+    [Benchmark]
+    public bool SpanContainSpan()
+    {
+        var role = "role".AsSpan();
+        return command.AsSpan().Contains(role, StringComparison.InvariantCultureIgnoreCase);
+    }
+}
